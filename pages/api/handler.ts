c6da501e132737867f60cb2844d3a00d5db79c64
youichi Ishigaki <deepestred1968@gmail.com>
2023-05-12 23:10:53 +0900

    second commit

--- pages/api/handler.ts
+++ pages/api/handler.ts
@@ -1,7 +1,7 @@
 // api/index.ts
-import { NowRequest, NowResponse } from "@now/node";
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default (req: NowRequest, res: NowResponse) => {
+export default function handler(req: VercelRequest, res: VercelResponse)  {
   const nodeVar = process.env.NODE_VAR || "no value"
   console.log({nodeVar})
   res.status(200).send(`Hello Vercel!! ${nodeVar}`);
